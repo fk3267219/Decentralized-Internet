3677ad2b78226f5da4277cc74b4e7f569bd5c2d6
fk3267219 <66290584+fk3267219@users.noreply.github.com>
2020-06-10 23:55:49 +0500

    Update AsyncResult.hx

--- gridbee-framework-old/henkolib/henkolib/async/AsyncResult.hx
+++ gridbee-framework-old/henkolib/henkolib/async/AsyncResult.hx
@@ -10,7 +10,7 @@
 //
 // GridBee is distributed in the hope that it will be useful
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNERPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
